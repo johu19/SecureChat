26d6a3a1a6e1e0cc8cd534f2de98e6ae56c4b38d
1151963652 <1151963652@DESKTOP-0GRQQ27>
2020-05-09 14:13:55 -0500

    comments

--- SaveChat/src/Client/ThreadCommunicationClient.java
+++ SaveChat/src/Client/ThreadCommunicationClient.java
@@ -43,24 +43,40 @@ public class ThreadCommunicationClient extends Thread {
 					cond = false;
 				}
 
+
 				if (client.getnClient() == 1) {
-
+					//En caso de que el cliente haya sido el primero en conectarse al servidor
+					
 					@SuppressWarnings("resource")
 					Scanner keyboard = new Scanner(System.in);
 					System.out.print("Tu: ");
+					
+					//Obtiene el mensaje escrito por el cliente en la consola
 					String message = keyboard.nextLine();
+					
+					//Envia al servidor el mensaje
 					out.writeUTF(client.getName() + ": " + message);
+					
+					//Recibe del servidor el mensaje que haya sido enviado por el otro cliente
 					String newMessage = in.readUTF();
 					System.out.println(newMessage);
 
+					
 				} else if (client.getnClient() == 2) {
+					//En caso de que el cliente haya sido el segundo en conectarse al servidor
 
+					
+					//Recibe del servidor el mensaje que haya sido enviado por el otro cliente
 					String newMessage = in.readUTF();
 					System.out.println(newMessage);
 					@SuppressWarnings("resource")
 					Scanner keyboard = new Scanner(System.in);
 					System.out.print("Tu: ");
+					
+					//Obtiene el mensaje escrito por el cliente en la consola
 					String message = keyboard.nextLine();
+					
+					//Recibe del servidor el mensaje que haya sido enviado por el otro cliente
 					out.writeUTF(client.getName() + ": " + message);
 
 				}
